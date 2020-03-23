d31907bd51d7caa79d20bdd99a65d370145f3dd4
vladimir666 <jovanovicv90@gmail.com>
2020-03-23 13:07:16 +0100

    Add HMR, connect store with App component, add typedef for webpack

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import configureStore from './state/index'
+import './index.scss'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore()
+
+const renderApp = () => {
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  )
+}
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept('./App', renderApp)
+}
+
+renderApp()
